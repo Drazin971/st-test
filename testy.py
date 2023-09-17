082c5340ae09ff4029781e807fa1dbc0d720ed3e
Drazin971 <a.dudek@gmail.com>
2023-09-17 13:58:56 +0200

    final test

--- testy.py
+++ testy.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import time
+import requests
 
 st.title("Streamlit on Replit")
 
@@ -164,3 +165,60 @@ selected_val = form.slider('Select a value')
 form.form_submit_button('Submit')
 
 st.write('Selected value: ', selected_val)
+
+st.title('st.experimental_get_query_params')
+
+with st.expander('About this app'):
+  st.write("`st.experimental_get_query_params` allows the retrieval of query parameters directly from the URL of the user's browser.")
+
+# 1. Instructions
+st.header('1. Instructions')
+st.markdown('''
+In the above URL bar of your internet browser, append the following:
+`?firstname=Jack&surname=Beanstalk`
+after the base URL `http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/`
+such that it becomes 
+`http://share.streamlit.io/dataprofessor/st.experimental_get_query_params/?firstname=Jack&surname=Beanstalk`
+''')
+
+
+# 2. Contents of st.experimental_get_query_params
+st.header('2. Contents of st.experimental_get_query_params')
+st.write(st.experimental_get_query_params())
+
+
+# 3. Retrieving and displaying information from the URL
+st.header('3. Retrieving and displaying information from the URL')
+
+firstname = st.experimental_get_query_params()['firstname'][0]
+surname = st.experimental_get_query_params()['surname'][0]
+
+st.write(f'Hello **{firstname} {surname}**, how are you?')
+
+st.title('🏀 Bored API app')
+
+st.sidebar.header('Input')
+selected_type = st.sidebar.selectbox('Select an activity type', ["education", "recreational", "social", "diy", "charity", "cooking", "relaxation", "music", "busywork"])
+
+suggested_activity_url = f'http://www.boredapi.com/api/activity?type={selected_type}'
+json_data = requests.get(suggested_activity_url)
+suggested_activity = json_data.json()
+
+c1, c2 = st.columns(2)
+with c1:
+  with st.expander('About this app'):
+    st.write('Are you bored? The **Bored API app** provides suggestions on activities that you can do when you are bored. This app is powered by the Bored API.')
+with c2:
+  with st.expander('JSON data'):
+    st.write(suggested_activity)
+
+st.header('Suggested activity')
+st.info(suggested_activity['activity'])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+  st.metric(label='Number of Participants', value=suggested_activity['participants'], delta='')
+with col2:
+  st.metric(label='Type of Activity', value=suggested_activity['type'].capitalize(), delta='')
+with col3:
+  st.metric(label='Price', value=suggested_activity['price'], delta='')
